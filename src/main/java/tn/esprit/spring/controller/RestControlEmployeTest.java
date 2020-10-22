461f4b964835117016349bd0e1b6147ea2a1b388
Sadoklaoo <47226412+Sadoklaoo@users.noreply.github.com>
2020-10-22 18:06:22 +0100

    Create RestControlEmployeTest.java

---
+++ src/main/java/tn/esprit/spring/controller/RestControlEmployeTest.java
@@ -0,0 +1,94 @@
+package tn.esprit.spring.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RestControlEmployeTest {
+	
+	// Sadok
+
+	@Test
+	public void testajouterEmploye() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testmettreAjourEmailByEmployeId() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testaffecterEmployeADepartement() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testdesaffecterEmployeDuDepartement() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testajouterContrat() {
+		fail("Not yet implemented");
+	}
+	
+	// end Sadok
+	
+	// Chantouf
+	
+	@Test
+	public void testaffecterContratAEmploye() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetEmployePrenomById() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testdeleteEmployeById() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testdeleteContratById() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetNombreEmployeJPQL() {
+		fail("Not yet implemented");
+	}
+	
+	// end Chantouf
+	
+	// Kimo
+	@Test
+	public void testgetAllEmployeNamesJPQL() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetAllEmployeByEntreprise() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testmettreAjourEmailByEmployeIdJPQL() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testdeleteAllContratJPQL() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetSalaireByEmployeIdJPQL() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetSalaireMoyenByDepartementId() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testgetTimesheetsByMissionAndDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testgetAllEmployes() {
+		fail("Not yet implemented");
+	}
+}
